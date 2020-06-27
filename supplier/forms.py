92b74d580f9b565024816b4a4ae79e59d6a858be
aayush1204 <50989118+aayush1204@users.noreply.github.com>
2020-06-28 02:49:19 +0530

    Revert "Supplier changes"

--- supplier/forms.py
+++ supplier/forms.py
@@ -1,11 +1,22 @@
 from django.forms import ModelForm
 from shop.models import Product
 from admindashboard.models import addproductlist
-
+from django import forms
 
 class AddProductForm(ModelForm):
     class Meta:
 
         model = addproductlist
         fields = '__all__'
-        exclude = ['discount_price','discount_percent','discount_applied','is_approved','out_of_stock', 'supplier']
+        widgets = {
+            'product_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'product_description': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.TextInput(attrs={'class': 'form-control'}),
+            'product_sku': forms.TextInput(attrs={'class': 'form-control'}),
+            'product_price': forms.TextInput(attrs={'class': 'form-control'}),
+            'supplier': forms.Select(attrs={'class': 'form-control'}),
+            'supplier_username': forms.TextInput(attrs={'class': 'form-control'}),
+            # 'videofile': forms.FileInput(attrs={'class': 'form-control'}),
+            
+        }
+        exclude = ['discount_price','discount_percent','discount_applied','is_approved','out_of_stock']
